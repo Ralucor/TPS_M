72a0356ddf771bf34728dd43766366bac8735488
Ralucor <rluqcor0106@g.educaand.es>
2025-01-30 13:33:33 +0100

    Scope

--- Assets/Scripts/ProjectileControl.cs
+++ Assets/Scripts/ProjectileControl.cs
@@ -1,17 +1,19 @@
 
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProjectileControl : MonoBehaviour
 {
     Rigidbody rb ;
     [SerializeField] float power = 30;
+    GameObject mira;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {   mira = GameObject.FindGameObjectWithTag("Scope");
         rb = GetComponent<Rigidbody>();
-        rb.AddForce (transform.TransformDirection(Vector3.forward) * power,
+        rb.AddForce (transform.TransformDirection(Vector3.MoveTowards(transform.position, mira.transform.position, Time.deltaTime * power)),
         ForceMode.Impulse);
 
         Invoke("Destruction", 3);
--- Assets/Scripts/ShootingControl.cs
+++ Assets/Scripts/ShootingControl.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class ShootingControl : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
-    [SerializeField] GameObject mira;
+    // [SerializeField] GameObject mira;
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)){
-            Instantiate(projectile, transform.position, mira.transform.rotation );
+            Instantiate(projectile, transform.position, transform.rotation);
         }
     }
 }
