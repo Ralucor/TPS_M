d8c37bdf0b1ffc78794dda4a75c7c5c07937b9b7
RafitaTNT <84412193+RafitaTNT@users.noreply.github.com>
2025-02-08 14:02:44 +0100

    MontarNivel

--- Assets/Scripts/ControlPlayer2.cs
+++ Assets/Scripts/ControlPlayer2.cs
@@ -7,23 +7,15 @@ public class ControlPlayer2 : MonoBehaviour
     [SerializeField] float health = 100;
     [SerializeField] float dps = 20;
 
-
     void OntriggerStay(Collider other){
          if (other.CompareTag("damage")){
             health = health - dps * Time.deltaTime;
             healthBar.value = health;
          }
-    }
-
-
-      // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-       
-    }
-    // Update is called once per frame
-    void Update()
-    {
+         if(health<=0){
         
+         }
     }
+
+
 }
